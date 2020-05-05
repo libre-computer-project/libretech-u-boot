fae9e6c59dbdf1d3e2ae47c2a305bd476d6bc948
Da Xue <da@libre.computer>
2020-05-05 07:49:40 -0400

    aml-s905x-cc: board header: add boot menu items

--- include/configs/libretech-cc.h
+++ include/configs/libretech-cc.h
@@ -28,6 +28,17 @@
         "ramdisk_addr_r=0x13000000\0" \
         "lc_fdtfile=" CONFIG_DEFAULT_FDT_FILE "\0" \
         "fdtfile=amlogic/" CONFIG_DEFAULT_DEVICE_TREE ".dtb\0" \
+	"bootmenu_0=Boot=boot; echo \"Boot failed.\"; sleep 30; $menucmd\0" \
+	"bootmenu_1=Boot USB=run bootcmd_usb0; echo \"USB Boot failed.\"; sleep 5; $menucmd -1\0" \
+	"bootmenu_2=Boot SD Card=run bootcmd_mmc0; echo \"SD Card Boot failed.\"; sleep 5; $menucmd -1\0" \
+	"bootmenu_3=Boot eMMC=run bootcmd_mmc1; echo \"eMMC Boot failed.\"; sleep 5; $menucmd -1\0" \
+	"bootmenu_4=Boot PXE=run bootcmd_pxe; echo \"PXE Boot failed.\"; sleep 5; $menucmd -1\0" \
+	"bootmenu_5=Boot DHCP=run bootcmd_dhcp; echo \"DHCP Boot failed.\"; sleep 5; $menucmd -1\0" \
+	"bootmenu_6=eMMC USB Drive Mode=mmc list; if mmc dev 0; then echo \"Press Control+C to end USB Drive Mode.\"; ums 0 mmc 0:0; echo \"USB Drive Mode ended.\"; else echo \"eMMC not detected.\"; fi; sleep 5; $menucmd -1\0" \
+	"bootmenu_7=Detect USB Devices; if usb reset; then echo \"USB detection complete.\"; else echo \"USB detection failed.\"; fi; sleep 5; $menucmd -1\0" \
+	"bootmenu_8=Reboot=reset\0" \
+	"bootmenu_delay=30\0" \
+	"menucmd=bootmenu\0" \
         BOOTENV
 
 #include <configs/meson64.h>
