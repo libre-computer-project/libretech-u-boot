9a0eaecf7b4707e6645b293ebb42c6dd66da61ed
Da Xue <da@libre.computer>
2021-06-18 13:52:51 -0400

    configs-header: libre-computer: create libretech header

---
+++ include/configs/libretech.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Configuration for Libre Computer boards
+ * (C) Copyright 2021 Da Xue <da@libre.computer>
+ */
+
+#ifndef __LIBRETECH_CONFIG_H
+#define __LIBRETECH_CONFIG_H
+
+#endif /* __LIBRETECH_CONFIG_H */
