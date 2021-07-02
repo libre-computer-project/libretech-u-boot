5ea637d1ba9aa44c4d54e2255894abf98500d842
Da Xue <da@libre.computer>
2021-07-02 19:00:24 -0400

    configs-header: libre-computer: splash screen source env def

--- include/configs/libretech.h
+++ include/configs/libretech.h
@@ -7,4 +7,14 @@
 #ifndef __LIBRETECH_CONFIG_H
 #define __LIBRETECH_CONFIG_H
 
+#ifdef CONFIG_SPLASH_SCREEN
+#define SPLASH_ENV \
+	"splashimage=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
+	"splashpos=m,m\0" \
+	"splashfile=boot.bmp\0" \
+	"splashsource=mmc_fs\0"
+#else
+#define SPLASH_ENV
+#endif
+
 #endif /* __LIBRETECH_CONFIG_H */
