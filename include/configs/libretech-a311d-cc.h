568c95b385fe00cddc3c720b66efb624afe59ad1
Da Xue <da.xue@libretech.co>
2023-08-11 01:31:34 -0400

    header: configs: add aml-a311d-cc

---
+++ include/configs/libretech-a311d-cc.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Configuration for LibreTech CC
+ *
+ * Copyright (C) 2022 Da Xue
+ * Author: Da Xue <da@libre.computer>
+ */
+
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+#include <configs/libretech.h>
+
+#define MESON_DEVICE_SETTINGS \
+	SPLASH_ENV
+
+#include <configs/meson64.h>
+
+#endif /* __CONFIG_H */
