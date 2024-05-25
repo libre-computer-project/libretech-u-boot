d5173eff45740bfe239a3593f3401d97f856a307
Da Xue <da@libre.computer>
2024-05-24 23:13:16 -0400

    roc-rk3328-cc-v2: copy from v1

--- board/rockchip/evb_rk3328/evb-rk3328.c
+++ board/rockchip/evb_rk3328/evb-rk3328.c
@@ -19,3 +19,15 @@ int rk_board_late_init(void){
 	rk_3328_qos_init();
 	return 0;
 }
+
+#if IS_ENABLED(CONFIG_OF_BOARD_FIXUP)
+int board_fix_fdt(void *blob)
+{
+	int nodeoff = 0;
+
+	while ((nodeoff = fdt_node_offset_by_compatible(blob, 0,
+				"jedec,spi-nor")) >= 0) {
+		fdt_del_node(blob, nodeoff);
+	}
+}
+#endif
