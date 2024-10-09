e6064198395f7f89dd9f9039af15c2979ff8f9ac
Da Xue <da@libre.computer>
2024-10-09 08:39:50 -0400

    meson64 configs header: remove dhcp and pxe targets

--- include/configs/meson64.h
+++ include/configs/meson64.h
@@ -122,9 +122,7 @@
 	BOOT_TARGET_MMC(func) \
 	BOOT_TARGET_DEVICES_USB(func) \
 	BOOT_TARGET_NVME(func) \
-	BOOT_TARGET_SCSI(func) \
-	BOOT_TARGET_PXE(func) \
-	BOOT_TARGET_DHCP(func)
+	BOOT_TARGET_SCSI(func)
 #endif
 
 #define BOOTM_SIZE		__stringify(0x1700000)
