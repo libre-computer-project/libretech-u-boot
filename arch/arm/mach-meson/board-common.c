46e98fae17a710ce787780027345a81da8165241
Da Xue <da@libre.computer>
2024-10-09 08:50:11 -0400

    mach-meson board: add env bootdevice and load ini from bootdevice

--- arch/arm/mach-meson/board-common.c
+++ arch/arm/mach-meson/board-common.c
@@ -5,6 +5,8 @@
 
 #include <cpu_func.h>
 #include <fastboot.h>
+#include <fs.h>
+#include <ini.h>
 #include <init.h>
 #include <net.h>
 #include <asm/arch/boot.h>
@@ -21,6 +23,7 @@
 #include <efi_loader.h>
 #include <u-boot/crc.h>
 #include <splash.h>
+#include <vsprintf.h>
 
 #include <asm/psci.h>
 
@@ -137,6 +140,24 @@ static void meson_set_boot_source(void)
 	env_set("boot_source", source);
 }
 
+#ifdef CONFIG_SYS_MMC_ENV_DEV
+int mmc_get_env_dev(void)
+{
+	if (meson_get_boot_device() == BOOT_DEVICE_SD)
+		return 1;
+
+	return CONFIG_SYS_MMC_ENV_DEV;
+}
+#endif
+
+void env_set_bootdevice(void){
+	char *bootdev = simple_itoa(meson_get_boot_device() == BOOT_DEVICE_SD);
+	env_set("bootdevice", bootdev);
+#ifdef CONFIG_SPLASH_SOURCE
+	env_set("splashdevpart", bootdev);
+#endif
+}
+
 __weak int meson_board_late_init(void)
 {
 	return 0;
@@ -170,5 +191,19 @@ int splash_screen_prepare(void)
 		return splash_source_load(splash_locations,
 			ARRAY_SIZE(splash_locations)) && splash_video_logo_load();
 	return splash_video_logo_load();
+}
+#endif
+
+#ifdef CONFIG_CMD_INI
+void env_ini_load(){
+	int res;
+	char *load_addr;
+	loff_t size;
+	res = fs_set_blk_dev("mmc", env_get("bootdevice"), FS_TYPE_ANY);
+	if (res) return;
+	load_addr = (char *)hextoul(env_get("loadaddr"), NULL);
+	res = fs_read("boot.ini",load_addr, 0, 0, &size);
+	if (res) return;
+	ini_parse(load_addr, size, ini_handler, "");
 }
 #endif
