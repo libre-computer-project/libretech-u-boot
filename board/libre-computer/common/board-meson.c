998b432440e8bbc827e830b8e9f157538c642de8
Da Xue <da@libre.computer>
2025-07-08 11:44:21 -0400

    board: libre-computer: board-meson: expand dfu alts

--- board/libre-computer/common/board-meson.c
+++ board/libre-computer/common/board-meson.c
@@ -3,6 +3,8 @@
  * (C) Copyright 2025 Da Xue <da@libre.computer>
  */
 
+#define DEBUG
+
 #include <asm/arch/boot.h>
 #include <asm/arch/gx.h>
 #include <asm/arch/sm.h>
@@ -24,27 +26,20 @@
 #ifdef CONFIG_SYS_MMC_ENV_DEV
 int mmc_get_env_dev(void)
 {
-	struct udevice *dev;
-	struct mmc *mmc;
+	debug("%s\n", __func__);
 	int bootdevice_num = CONFIG_SYS_MMC_ENV_DEV;
-
 	switch (meson_get_boot_device()) {
 		case BOOT_DEVICE_EMMC:
 			break;
 
 		case BOOT_DEVICE_SPI:
+		case BOOT_DEVICE_USB:
 			/* TODO: eMMC, NVME, SD, USB */
-			if (uclass_get_device_by_seq(UCLASS_MMC, 1, &dev)) break;
-			if (!(mmc = mmc_get_mmc_dev(dev))) break;
-			if (!IS_SD(mmc) && !mmc->has_init)
-				bootdevice_num = 1;
+			if (!mmc_is_present(bootdevice_num))
+				bootdevice_num = mmc_is_present(!bootdevice_num);
 			break;
-
 		case BOOT_DEVICE_SD:
 			bootdevice_num = 1;
-			break;
-
-		case BOOT_DEVICE_USB:
 			break;
 	}
 
@@ -55,10 +50,12 @@ int mmc_get_env_dev(void)
 #endif
 
 #if IS_ENABLED(CONFIG_SET_DFU_ALT_INFO)
+#define DFU_ALT_INFO_ENV_LENGTH 512
+#define DFU_INTERFACE_SEPARATOR "&"
 #ifdef CONFIG_DFU_RAM
 #define MESON_DFU_RAM_ALTS \
 	"load ram " __stringify(CONFIG_SYS_LOAD_ADDR) " " __stringify(CONFIG_SYS_BOOTM_LEN) ";" \
-	"script ram " SCRIPT_ADDR_R " 0x8000;" \
+	"script script " SCRIPT_ADDR_R " 0x8000;" \
 	"fdt ram " FDT_ADDR_R " 0x78000;" \
 	"kernel ram " KERNEL_ADDR_R " 0x7f80000;" \
 	"ramdisk ram " RAMDISK_ADDR_R " 0x4000000"
@@ -75,12 +72,12 @@ int mmc_get_env_dev(void)
 #endif
 #ifdef CONFIG_DFU_MMC
 #define MESON_DFU_MMC_EMMC_ALTS \
-	"emmc raw 0 0 mmcpart 0;" \
-	"emmc-boot0 raw 0 0 mmcpart 1;" \
-	"emmc-boot1 raw 0 0 mmcpart 2;" \
 	"u-boot-bin-emmc raw 1 0x7ff mmcpart 0;" \
 	"u-boot-bin-emmc-boot0 raw 0x1 0x7ff mmcpart 1;" \
-	"u-boot-bin-emmc-boot1 raw 0x1 0x7ff mmcpart 2"
+	"u-boot-bin-emmc-boot1 raw 0x1 0x7ff mmcpart 2;" \
+	"emmc raw 0 0 mmcpart 0;" \
+	"emmc-boot0 raw 0 0 mmcpart 1;" \
+	"emmc-boot1 raw 0 0 mmcpart 2"
 #define MESON_DFU_MMC_EMMC "mmc 0=" MESON_DFU_MMC_EMMC_ALTS "&"
 
 #ifdef MESON_DFU_MMC_SD_HIDE
@@ -88,23 +85,20 @@ int mmc_get_env_dev(void)
 #define MESON_DFU_MMC_SD
 #else
 #define MESON_DFU_MMC_SD_ALTS \
-	"sd raw 0 0 mmcpart 0;" \
-	"u-boot-bin-sd raw 1 0x7ff mmcpart 0"
+	"u-boot-bin-sd raw 1 0x7ff mmcpart 0;" \
+	"sd raw 0 0 mmcpart 0"
 #define MESON_DFU_MMC_SD "mmc 1=" MESON_DFU_MMC_SD_ALTS "&"
 #endif
-#define MESON_DFU_MMC_CMD_ALTS "script 0 0"
-#define MESON_DFU_CMD "mmc 0=cmd script 0 0&"
 #else
 #define MESON_DFU_MMC_EMMC
 #define MESON_DFU_MMC_SD
-#define MESON_DFU_CMD
 #endif
 
 void meson_set_dfu_alt_info(char *interface, char *devstr)
 {
-	printf("%s: interface %s devstr %s\n", __func__, interface, devstr);
+	debug("%s: interface %s devstr %s\n", __func__, interface, devstr);
 	char *dfu_alt_info;
-	struct udevice *dev;
+	bool free_dfu_alt_info = false;
 	if (interface){
 #ifdef CONFIG_DFU_RAM
 		if (strcmp(interface, "ram") == 0)
@@ -116,38 +110,47 @@ void meson_set_dfu_alt_info(char *interface, char *devstr)
 #endif
 #ifdef CONFIG_DFU_MMC
 		if (strcmp(interface, "mmc") == 0){
-			if (!devstr)
-				dfu_alt_info = MESON_DFU_MMC_EMMC MESON_DFU_MMC_SD;
-			else if (strcmp(devstr, "0") == 0)
+			if (!devstr){
+				dfu_alt_info = malloc(DFU_ALT_INFO_ENV_LENGTH);
+				free_dfu_alt_info = true;
+				if (mmc_is_present(0))
+					strcat(dfu_alt_info, MESON_DFU_MMC_EMMC);
+#ifndef MESON_DFU_MMC_SD_HIDE
+				if (mmc_is_present(1))
+					strcat(dfu_alt_info, MESON_DFU_MMC_SD);
+#endif
+			}
+			else if (strcmp(devstr, "0") == 0 && mmc_is_present(0))
 				dfu_alt_info = MESON_DFU_MMC_EMMC_ALTS;
 #ifndef MESON_DFU_MMC_SD_HIDE
-			else if (strcmp(devstr, "1") == 0)
+			else if (strcmp(devstr, "1") == 0 && mmc_is_present(1))
 				dfu_alt_info = MESON_DFU_MMC_SD_ALTS;
 #endif
 		}
 #endif
 	} else {
-		dfu_alt_info = malloc(1024);
+		dfu_alt_info = malloc(DFU_ALT_INFO_ENV_LENGTH);
+		free_dfu_alt_info = true;
+#ifdef CONFIG_DFU_SF
 		strcpy(dfu_alt_info, MESON_DFU_SF);
-		strcat(dfu_alt_info, MESON_DFU_CMD);
-
-		if (uclass_get_device(UCLASS_MMC, 0, &dev) == 0 && mmc_get_mmc_dev(dev)){
+#endif
+		if (mmc_is_present(0))
 			strcat(dfu_alt_info, MESON_DFU_MMC_EMMC);
-		}
-		if (uclass_get_device(UCLASS_MMC, 1, &dev) == 0 && mmc_get_mmc_dev(dev)){
-			strcat(dfu_alt_info, MESON_DFU_MMC_SD);
-		}
+#ifndef MESON_DFU_MMC_SD_HIDE
+		if (mmc_is_present(1))
+					strcat(dfu_alt_info, MESON_DFU_MMC_SD);
+#endif
 		strcat(dfu_alt_info, MESON_DFU_RAM);
 	}
 	
 	if (dfu_alt_info)
 		env_set("dfu_alt_info", dfu_alt_info);
+	if (free_dfu_alt_info)
+		free(dfu_alt_info);
 }
 #endif
 
 static int settings_r(void){
-	struct udevice *dev;
-	struct mmc *mmc;
 	int bootdevice_num = CONFIG_SYS_MMC_ENV_DEV;
 	
 	char *bootsource;
@@ -161,11 +164,8 @@ static int settings_r(void){
 		case BOOT_DEVICE_SPI:
 			/* TODO: eMMC, NVME, SD, USB */
 			bootsource = "mmc";
-			/* no longer HW order */
-			if (uclass_get_device_by_seq(UCLASS_MMC, 0, &dev)) break;
-			if (!(mmc = mmc_get_mmc_dev(dev))) break;
-			if (!IS_SD(mmc) && !mmc->has_init)
-				bootdevice_num = 1;
+			if (!mmc_is_present(bootdevice_num))
+				bootdevice_num = mmc_is_present(!bootdevice_num);
 			break;
 
 		case BOOT_DEVICE_SD:
